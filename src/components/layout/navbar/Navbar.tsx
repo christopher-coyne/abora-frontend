359c4c9b00e5e91abb8919c362cb5fff6bb343dc
christopher coyne <christcoyne@gmail.com>
2023-01-18 22:08:48 -0500

    project scaffolding

---
+++ src/components/layout/navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+import { Nav } from './styles';
+
+export function Navbar() {
+  return <Nav>Navbar</Nav>;
+}
---
+++ src/components/layout/navbar/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  padding: 20px 10px;
+  display: flex;
+`;
